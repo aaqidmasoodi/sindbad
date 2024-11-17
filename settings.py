b63b300263f7cfdbe2f74d3925cf5f6070231f6c
aaqidmasoodi <aaqidmasoodi@icloud.com>
2024-11-17 00:16:43 +0000

    first commit

---
+++ settings.py
@@ -0,0 +1,12 @@
+import pygame, sys
+from pygame.math import Vector2 as vector
+
+GAME_TITLE = 'Pygame Platformer'
+WINDOW_WIDTH, WINDOW_HEIGHT = 1280, 720
+TILE_SIZE = 64
+ANIMATION_SPEED = 6
+
+#Layers
+Z_LAYERS = {
+
+}
